10d8fc29ee2d9a3dcb21a6d356520527a7cba701
mehediemon007 <mehediemon7@gmail.com>
2022-02-24 14:02:52 +0600

    first

---
+++ js/main.js
@@ -0,0 +1,26 @@
+;(function($){
+    // Banner
+
+    $(".arr-banner").owlCarousel({
+        items:1,
+        nav:false,
+        animateOut: 'fadeOut',
+        smartSpeed:300,
+        autoplay:true
+    })
+
+    // Arrival Slider
+
+    $(".dress-slider").owlCarousel({
+        items:4,
+        margin:10,
+        nav:true,
+        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    })
+
+})(jQuery);
+
+(function(){
+
+
+})()
